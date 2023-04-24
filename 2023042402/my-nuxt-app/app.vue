f29d9ce10604abc3b86c92400ee1445553c9e37d
wanyutang <wanyutang@gmail.com>
2023-04-24 09:45:35 +0000

    2023042402 Nuxt3 預設 NuxtWelcome 與 NuxtPage

---
+++ 2023042402/my-nuxt-app/app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Hello Nuxt3! This is App</h1>
+    <NuxtPage />
+  </div>
+</template>
---
+++ 2023042402/my-nuxt-app/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Hello Nuxt3! This is Index ! </h1>
+    <p>This is my first Nuxt3 app.</p>
+  </div>
+</template>
